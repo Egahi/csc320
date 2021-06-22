ff2deb3bdf9e98a128df2c5f638eee497d8d729a
Egahi S. O <okworisamuel6@gmail.com>
2021-06-22 09:13:58 +0100

    Add recursive version

--- InsertionSort.py
+++ InsertionSort.py
@@ -3,15 +3,16 @@ Insertion Sort
 
 Given an array, A
 
-for i = 2 to A.length
-    key = a[i]
-    // insert key into sorted array A[1..i-1]
-    j = i - 1
-    while j > 0 and A[j] > key
-        A[j + 1] = A[j]
-        j = j - 1
+InsertionSort(A)
+    for i = 2 to A.length
+        key = a[i]
+        // insert key into sorted array A[1..i-1]
+        j = i - 1
+        while j > 0 and A[j] > key
+            A[j + 1] = A[j]
+            j = j - 1
 
-    A[j + 1] = key
+        A[j + 1] = key
 
 
 Runtime:
@@ -32,6 +33,19 @@ class InsertionSort:
             
             nums[j + 1] = temp
 
+    def sort_asc_recursively(self, nums):
+        if not nums or len(nums) <= 1:
+            return
+
+        self.sort_asc_recursively(nums[:-1])
+
+        temp = nums[-1]
+        idx = len(nums) - 2
+        while idx >= 0 and nums[idx] > temp:
+            nums[idx + 1] = nums[idx]
+            idx -= 1
+        
+        nums[idx + 1] = temp
     
     def sort_descending(self, nums):
         for i in reversed(range(0, len(nums) - 1)):
@@ -46,8 +60,12 @@ class InsertionSort:
 
 obj = InsertionSort()
 nums = [5,2,4,6,1,3]
+print(nums)
 print('Sorted in ascending order')
 obj.sort_ascending(nums)
+print(nums)
+print('Sorted recursively in ascending order')
+obj.sort_asc_recursively(nums)
 print(nums)
 print('Sorted in descending order')
 obj.sort_descending(nums)
